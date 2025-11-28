9154733a5af08443b30d76ae2437a3c678086537
vivianemenezes571 <vivianemenezes5711@gmail.com>
2025-11-28 19:52:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿const string FraseParaSair = "tchau";
+
+Console.WriteLine("--- Groot Chatbot ---\n");
+
+string pergunta;
+do
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!;
+
+    string resposta = ObtemRespostaGroot(pergunta);
+
+    ExibeResposta(ObtemRespostaGroot(pergunta));
+
+} while (pergunta.Trim().ToLower() != FraseParaSair);
+
+
+void ExibeResposta(string resposta)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"                          Resposta: {resposta}");
+    Console.ResetColor();
+}
+
+string ObtemRespostaGroot(string pergunta)
+{
+    return pergunta.Trim().ToLower() == FraseParaSair ?
+        "Eu sou Groot!" :
+        "Eu sou Groot.";
+}
